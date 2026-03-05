8bb286cb2101e5e2ca098df9a7550872b080f40d
boxp <tiyotiyouda@gmail.com>
2026-03-05 13:01:13 +0900

    fix(formula): avoid URL resolution failure for ceeker

--- Formula/ceeker.rb
+++ Formula/ceeker.rb
@@ -8,33 +8,34 @@ class Ceeker < Formula
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/boxp/ceeker/releases/download/v0.1.3/ceeker-darwin-arm64.tar.gz"
-      sha256 "442850514610f9be4913133640e5fbf3295178b278f0526149b7515bf3f90cff"
-
-      def install
-        bin.install "ceeker-darwin-arm64" => "ceeker"
-      end
-    end
+    url "https://github.com/boxp/ceeker/releases/download/v0.1.3/ceeker-darwin-arm64.tar.gz"
+    sha256 "442850514610f9be4913133640e5fbf3295178b278f0526149b7515bf3f90cff"
   end
 
   on_linux do
-    on_intel do
-      url "https://github.com/boxp/ceeker/releases/download/v0.1.3/ceeker-linux-amd64.tar.gz"
-      sha256 "be28b2185deca4de62128682a60e161b37c87ce24aeb1a04df03c162fd90bb0a"
+    url "https://github.com/boxp/ceeker/releases/download/v0.1.3/ceeker-linux-amd64.tar.gz"
+    sha256 "be28b2185deca4de62128682a60e161b37c87ce24aeb1a04df03c162fd90bb0a"
 
-      def install
-        bin.install "ceeker-linux-amd64" => "ceeker"
-      end
-    end
     on_arm do
       url "https://github.com/boxp/ceeker/releases/download/v0.1.3/ceeker-linux-arm64.tar.gz"
       sha256 "e8dd0d2c39e3f4225a808dca7395ce5ce75fede55e5b7895bc5491671b353c77"
-
-      def install
-        bin.install "ceeker-linux-arm64" => "ceeker"
-      end
     end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.intel?
+      odie "ceeker does not currently provide macOS Intel binaries."
+    end
+
+    binary = if OS.mac?
+      "ceeker-darwin-arm64"
+    elsif File.exist?("ceeker-linux-arm64")
+      "ceeker-linux-arm64"
+    else
+      "ceeker-linux-amd64"
+    end
+
+    bin.install binary => "ceeker"
   end
 
   test do
